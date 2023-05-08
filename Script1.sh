38d0f884a915e5ea21b4d057768999431288435e
2023Manoj <132283765+2023Manoj@users.noreply.github.com>
2023-05-08 15:34:22 +0530

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -7,8 +7,8 @@ echo "Best class of AWS/DevOps"
 echo "We are learning git"
 echo "We are done with git"
 echo "This example is of branching strategy"
-echo "Sweet Hearts"
-echo "Sweet Dream"
+echo "This is Afternoon"
+echo "This is Evening"
 
 
 
